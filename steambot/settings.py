5ba762c70c1cb56f10b6ebf9b3d346eeadbef51e
Manuel <manuel.marino03@gmail.com>
2020-06-02 15:02:42 -0400

    app steam

--- steambot/settings.py
+++ steambot/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+APPS = ['apps.steam']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
